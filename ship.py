f6a94a5559567859452c7ff6fac00444173de48f
Iowan <dyakin_ip@outlook.com>
2017-06-13 21:37:33 +0300

    Ship's speed
    add parametr

--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self,ai_settings, screen):
         """Init ship and it's position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         """Downloading img ship and takeing square"""
         self.image = pygame.image.load('images/ship.bmp')
@@ -13,16 +14,22 @@ class Ship():
         """New ship creats on bottom"""
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        """Saving float coordinate of ship's center"""
+        self.center = float(self.rect.centerx)
 
         """Moving flag"""
         self.moving_right = False
         self.moving_left = False
+
     def update(self):
         """Updating position of ship"""
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
+
     def blitme(self):
         """Blitting ship"""
         self.screen.blit(self.image,self.rect)
