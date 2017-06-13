f7783cf123c9190a66da11ea50ac20b2bfac636a
Iowan <dyakin_ip@outlook.com>
2017-06-13 20:47:33 +0300

    Movings
    First moves of ship(only Right)

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,6 @@ def run_game():
     pygame.display.set_caption("Alien Invasion v0")
     ship = Ship(screen)
     while True:
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings,screen,ship)
 run_game()
