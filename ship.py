b1e8716635739fba63bb34e7d702e1d414340350
Iowan <dyakin_ip@outlook.com>
2017-06-13 21:01:21 +0300

    No-stopping moving
    Moving to two sides without stopping

--- ship.py
+++ ship.py
@@ -9,9 +9,20 @@ class Ship():
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
+
         """New ship creats on bottom"""
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        """Moving flag"""
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        """Updating position of ship"""
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
     def blitme(self):
         """Blitting ship"""
         self.screen.blit(self.image,self.rect)
