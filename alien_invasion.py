ce0b51745b81c384a060e8d6f39e39e25b5ecef3
Iowan <dyakin_ip@outlook.com>
2017-06-14 12:43:16 +0300

    ConsoleKeyLogger
    KeyLogger

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,14 +6,17 @@ import pygame
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption("Alien Invasion v0")
-
+    pygame.display.set_caption("KeyLogger")
+    bg_color = (230,230,230)
+    print(screen)
     while True:
         #Checking keydowns
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-    #Flipping display
-    pygame.display.flip()
+            if pygame.KEYDOWN:
+                print(str(event) + "\n")
+        screen.fill(bg_color)
+        #Flipping display
+        pygame.display.flip()
 run_game()
